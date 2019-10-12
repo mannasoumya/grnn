a54f25fac79b28e08619bdd417243397934fab9f
Manna Soumya <sm323265@gmail.com>
2019-10-12 14:10:45 +0530

    Major Update Converted to Class

--- grnn.js
+++ grnn.js
@@ -2,56 +2,87 @@
 INPUT PARAMETERS:
 train_x   : 2d array of n rows(training size) and m columns(features) :: data type --> double
 train_y   : 1d array of size n (actual output correspondng to each training input) :: data type --> double
+test_x   : 2d array of n1 rows(testing size) and m columns(features) :: data type --> double
+test_y   : 1d array of size n (actual output correspondng to each testing input) :: data type --> double
 input     : 1d array of size n (input data whose Y needs to be predicted) :: data type --> double
 sigma     : the value of sigma in the Radial Basis Function (defaults to √2):: data type --> double
 normalize : whether to normalize train_x or not (generally normalization of training samples gives better predictions) :: data type --> boolean
-*/
 
-module.exports = function(train_x, train_y, input, sigma, normalize) {
-  //function to calculate Radial Basis Function Kernel value
-  function rbf(diff, sig) {
+*/
+class GRNN {
+  constructor(train_x, train_y, sigma, normalize, test_x, test_y) {
+    this.train_x = train_x;
+    this.train_y = train_y;
+    this.sigma = sigma;
+    this.normalize = normalize;
+    this.test_x = test_x;
+    this.test_y = test_y;
+    this.ypred = [];
+  }
+  rbf(diff, sig) {
     if (sig != undefined && sig != 0) {
       let coeff = (-0.5 * diff * diff) / (sig * sig);
       return Math.exp(coeff);
     } else {
       return Math.exp(-1 * diff * diff);
     }
-  } //end of rbf function
+  }
   //function to normalize an one dimensional array
-  function normalize_1d_array(arr) {
+  normalize_1d_array(arr) {
     let ss = 0;
     for (let i = 0; i < arr.length; i++) {
       ss = ss + arr[i] * arr[i];
     }
     return arr.map(x => x / Math.sqrt(ss));
-  } // end of normalize_1d_array function
-  if (normalize == true) {
-    for (let x of train_x) {
-      x = normalize_1d_array(x);
+  }
+  //function to check normalization switch
+  checkNormal(normalize, tr_ts_x) {
+    if (normalize == true) {
+      for (let x of tr_ts_x) {
+        x = this.normalize_1d_array(x);
+      }
     }
   }
-  //function to predict output give training input using GRNN
-  function predict(train_x, train_y, input) {
+  //function to predict output given training input using GRNN
+  prediction(train_x, train_y, input) {
+    this.checkNormal(this.normalize, this.train_x);
     if (train_x.length != train_y.length) {
       throw new Error("Shape mismatch");
     } else {
       let num = 0,
         den = 0,
         sig_ma = Math.sqrt(2);
-      if (sigma != undefined) {
-        sig_ma = sigma;
+      if (this.sigma != undefined) {
+        sig_ma = this.sigma;
       }
       for (let i = 0; i < train_x.length; i++) {
         let sum = 0;
         for (let j = 0; j < train_x[0].length; j++) {
-          let rbf_temp = rbf(input[j] - train_x[i][j], sig_ma);
+          let rbf_temp = this.rbf(input[j] - train_x[i][j], sig_ma);
           sum = sum + rbf_temp;
         }
         num = num + train_y[i] * sum;
         den = den + sum;
       }
       return num / den;
-    } // end of predict function
+    }
   }
-  return predict(train_x, train_y, input);
-}; // end of module export
+  //wrapper like function over prediction(...) function
+  predict(input) {
+    return this.prediction(this.train_x, this.train_y, input);
+  }
+  //function to calculate mean square error on test data
+  mse() {
+    this.checkNormal(this.normalize, this.test_x);
+    let sum = 0;
+    for (let x of this.test_x) {
+      this.ypred.push(this.predict(x));
+    }
+    for (let i = 0; i < this.ypred.length; i++) {
+      let err = this.test_y[i] - this.ypred[i];
+      sum = sum + err * err;
+    }
+    return sum / this.ypred.length;
+  }
+}
+module.exports = GRNN;
